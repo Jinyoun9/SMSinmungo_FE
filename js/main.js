26062647cbec5e023be33205cc299f58dbe6fd5f
lsc2159 <lsc2159@naver.com>
2024-11-13 17:13:16 +0900

    ✨feat:main 틀 조금

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function gotosignUp() {
+  window.location.href = "signUp.html";
+}
+function gotologin() {
+  window.location.href = "login.html";
+}
+function gotoMain() {
+  window.location.href = "main.html";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  // page-frame2를 선택
+  const pageFrame2 = document.querySelector(".page-frame2");
+
+  // 클릭 이벤트 추가
+  pageFrame2.addEventListener("click", function () {
+    alert("page-frame2가 클릭되었습니다!");
+    // 원하는 동작 추가 가능
+  });
+});
