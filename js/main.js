7f9527cedcc80209f30d7b6220bfcd55966df2d8
lsc2159 <lsc2159@naver.com>
2024-11-19 20:20:31 +0900

    ✨feat:mainWrite 구성하기

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ function gotologin() {
 function gotoMain() {
   window.location.href = "main.html";
 }
+function gotoWrite() {
+  window.location.href = "mainWrite.html";
+}
 document.addEventListener("DOMContentLoaded", function () {
   // page-frame2를 선택
   const pageFrame2 = document.querySelector(".page-frame2");
@@ -15,5 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
   pageFrame2.addEventListener("click", function () {
     alert("page-frame2가 클릭되었습니다!");
     // 원하는 동작 추가 가능
+  });
+});
+
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+  item.addEventListener("click", function (event) {
+    const selectedText = event.target.textContent;
+    const dropdownButton = document.getElementById("dropdownButton");
+    dropdownButton.textContent = selectedText;
   });
 });
